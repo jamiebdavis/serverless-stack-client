155f46fe99faf5fca692c28bbfc6d3316e8afe32
Jamie Davis <jamie.davis@ao.com>
2020-12-24 10:35:16 +0000

    added auth to app

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,22 @@
 import { Route, Switch } from "react-router-dom";
 
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 import React from "react";
+import Signup from "./containers/Signup";
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/">
                 <Home />
+            </Route>
+            <Route exact path="/login">
+                <Login />
+            </Route>
+            <Route exact path="/signup">
+                <Signup />
             </Route>
             {/* Finally, catch all unmatched routes */}
             <Route>
---
+++ src/components/LoaderButton.js
@@ -0,0 +1,14 @@
+import "./LoaderButton.css";
+
+import { BsArrowRepeat } from "react-icons/bs";
+import Button from "react-bootstrap/Button";
+import React from "react";
+
+export default function LoaderButton({ isLoading, className = "", disabled = false, ...props }) {
+    return (
+        <Button disabled={disabled || isLoading} className={`LoaderButton ${className}`} {...props}>
+            {isLoading && <BsArrowRepeat className="spinning" />}
+            {props.children}
+        </Button>
+    );
+}
